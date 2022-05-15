1585f9327684f12b65974886d6fa19b03cf170fa
Evan Chernicky <echernicky@gmail.com>
2022-05-15 14:59:52 -0400

    added aws

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ Rails.application.configure do
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
 
-  config.active_storage.service = :local
+  config.active_storage.service = :amazon
 
 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
