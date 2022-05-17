d96a0adc4defd734839ffbf4df9586812040f759
Evan Chernicky <echernicky@gmail.com>
2022-05-17 16:35:10 -0400

    added more font sizes

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -8,8 +8,10 @@ module.exports = {
       xl: '1440px'
     },
     fontSize: {
+      'lg': 'clamp(.75rem, 1.75vw, 1.125rem)',
       'xl': 'clamp(.9rem, 2vw, 1.25rem)',
       '2xl': 'clamp(1.15rem, 2.5vw, 1.5rem)',
+      '3xl': 'clamp(1.25rem, 3vw, 1.875rem)',
       '4xl': 'clamp(1.5rem, 4vw, 2.25rem)',
       '5xl': 'clamp(1.75rem, 5vw, 3rem)',
       '6xl': 'clamp(2rem, 6vw, 4rem)'
